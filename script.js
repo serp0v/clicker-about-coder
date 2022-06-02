ebec2d8792b69a31aabd731bf2a5dcf54b0ed4a5
serp0v <serpov.cc@mail.ru>
2022-06-02 10:25:10 +1000

    add a metrics prototyle n clean code

--- script.js
+++ script.js
@@ -1,50 +1,23 @@
 ////////////////////////////////////Черкасов Александр//////////////////////////////////////////////////
 /////////////////////////////////////vk:@danilserpov////////////////////////////////////////////////////
 
-
-
-//  navbar
+//navbar
 
 let menushka = document.getElementById('menushka');
 let mainbar = document.getElementById('mainbar');
 
 // юзануть forEach, ускорит код 
 
-const navbarFirst = document.querySelector('#navbar1')
-const navbarSecond = document.querySelector('#navbar2')
-const navbarThird = document.querySelector('#navbar3')
-const navbarFourth = document.querySelector('#navbar4')
-const navbarFifth = document.querySelector('#navbar5')
-
-// const accoChildren = document.querySelectorAll('.accordionClose');
-// accoChildren.forEach(b=>b.addEventListener('click', mainbarSwitcher()));
-
-mainbar.onclick = function() {
+const accoChildren = document.querySelectorAll('.accordionClose');
+accoChildren.forEach(accoChildren=>mainbar.addEventListener('click', function(){
     mainbar.classList.toggle('mainbarClose');
     mainbar.classList.toggle('mainbarOpen');
 
-    navbarFirst.classList.toggle('accordionClose');
-    navbarSecond.classList.toggle('accordionClose');
-    navbarThird.classList.toggle('accordionClose');
-    navbarFourth.classList.toggle('accordionClose');
-    navbarFifth.classList.toggle('accordionClose');
-
-    //это пишется в две строки
-
-    navbarFirst.classList.toggle('accordionOpen');
-    navbarSecond.classList.toggle('accordionOpen');
-    navbarThird.classList.toggle('accordionOpen');
-    navbarFourth.classList.toggle('accordionOpen');
-    navbarFifth.classList.toggle('accordionOpen');
-};
-
-
-
+    accoChildren.classList.toggle('accordionClose');
+    accoChildren.classList.toggle('accordionOpen');
+}));
 
 // pre Text Closer(i'm a guest)
-
-
-
 
 const closeGuest = document.querySelector('#amaGuest');
 const preText = document.querySelector('#pre');
@@ -63,3 +36,4 @@ closeGuest.onclick = function preMenuCloser() {
     contentWrapper.classList.add('contentWrapAnimation');
 
 }
+// 
